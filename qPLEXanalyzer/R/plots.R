a14fde2a30f875964eed35f62d92ecce760b5110
Ash Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2018-04-20 15:17:01 +0100

    Tidying of the peptideIntensities plot code
    Former-commit-id: 6af3e80417ec375924283f681f9faeab5af97b54

--- qPLEXanalyzer/R/plots.R
+++ qPLEXanalyzer/R/plots.R
@@ -70,6 +70,7 @@ peptideIntensityPlot <- function(MSnSetObj, ProteinID, ProteinName,
       stop('combinedIntensities has to either NULL or object of class MSnSet..')
   }
   
+  ## get peptide intensities for plotting
   intensities <- exprs(MSnSetObj) %>% 
     as.data.frame() %>% 
     rownames_to_column("PeptideID") %>%
@@ -78,11 +79,13 @@ peptideIntensityPlot <- function(MSnSetObj, ProteinID, ProteinName,
     mutate(logIntensity=log2xplus1(Intensity)) %>% 
     filter(Accessions == ProteinID)
   
+  ## get intensities for selected sequences if present
   seqIntensities <-  filter(intensities, Sequences %in% selectedSequence)
   if (!is.null(selectedModifications)){ 
     seqIntensities %<>% filter(Modifications %in% selectedModifications)
   }
   
+  ## get intensity for protein level (summarised) data if present
   summIntensities <- data.frame(SampleName=vector(), logIntensity=vector(), Protein=vector())
   if (!is.null(combinedIntensities)){
     summIntensities <- exprs(combinedIntensities) %>% 
@@ -99,21 +102,18 @@ peptideIntensityPlot <- function(MSnSetObj, ProteinID, ProteinName,
     return(NULL)
   }
   
-  ggplot(intensities) +
-    geom_line(aes(x=SampleName, y=logIntensity, group=PeptideID, colour=Sequences),
-              size=0.6, alpha=0.5, linetype=2) + 
-    geom_point(aes(x=SampleName, y=logIntensity, fill=Sequences), shape=21, size=2) + 
-    geom_line(data=seqIntensities, aes(x=SampleName, y=logIntensity, group=PeptideID),
-              colour="#6666FF", size=1.2, linetype=6)  + 
-    geom_point(data=seqIntensities, aes(x=SampleName, y=logIntensity),
-               fill="#0000FF", shape=21, size=2.5) + 
-    geom_line(data=summIntensities, aes(x=SampleName, y=logIntensity, group=Protein),
-              colour="#AAAAAA", size=1.2, linetype=6) +
-    geom_point(data=summIntensities, aes(x=SampleName, y=logIntensity),
-               fill="#888888", shape=21, size=2.5) + 
+  ggplot(intensities, aes(x=SampleName, y=logIntensity)) +
+    geom_line(aes(group=PeptideID, colour=Sequences), size=0.6, alpha=0.5, linetype=2) +
+    geom_point(aes(fill=Sequences), shape=21, size=2) +
+    ## plot the selected sequences if present
+    geom_line(data=seqIntensities, aes(group=PeptideID), colour="#6666FF", size=1.2, linetype=6)  +
+    geom_point(data=seqIntensities, fill="#0000FF", shape=21, size=2.5) +
+    ## plot the summarised intensities if present
+    geom_line(data=summIntensities, aes(group=Protein), colour="#AAAAAA", size=1.2, linetype=6) +
+    geom_point(data=summIntensities, fill="#888888", shape=21, size=2.5) +
     labs(y="log2(Intensity)", title=ProteinName) +
     theme_bw() +
-    theme(plot.title=element_text(size=14,hjust=0.5),
+    theme(plot.title=element_text(size=14, hjust=0.5),
           axis.text.x=element_text(size=11, angle=45, hjust=1),
           axis.title.x=element_blank(),
           axis.text.y=element_text(size=12),
