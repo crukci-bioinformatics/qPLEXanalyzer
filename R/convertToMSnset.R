907c7069d7840f177972749ebfa88a167f7e3ff6
Ashley Sawle <ads2202cu@gmail.com>
2020-08-26 17:29:51 +0100

    tidy the code a bit

--- R/convertToMSnset.R
+++ R/convertToMSnset.R
@@ -7,20 +7,20 @@ convertToMSnset <- function(ExpObj, metadata, indExpData, Sequences=NULL,
     colnames(ExpObj)[Sequences] <- "Sequences"
     
     if (rmMissing) {
-        ExpObj %<>% filter(across(indExpData, ~!is.na(.x)))
+        ExpObj <- ExpObj %>% 
+            drop_na(indExpData)
     }
     obj <- readMSnSet2(ExpObj, ecol = indExpData)
 
-    # if the meta data is a tibble, we can't set rownames
-    metadata <- as.data.frame(metadata)
-    rownames(metadata) <- as.character(metadata$SampleName)
-    ind <- match(sampleNames(obj), rownames(metadata))
-    metadata <- metadata[ind, ]
-    pData(obj) <- metadata
+    pData(obj) <- metadata %>% 
+        mutate(rowname = SampleName) %>% 
+        column_to_rownames() %>% 
+        `[`(sampleNames(obj), TRUE)
+            
     if (type == "protein") {
         featureNames(obj) <- fData(obj)$Accessions
     } else {
-        featureNames(obj) <- paste0("peptide_", featureNames(obj))
+        featureNames(obj) <- str_c("peptide_", featureNames(obj))
     }
     return(obj)
 }
