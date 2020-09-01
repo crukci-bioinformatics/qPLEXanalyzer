0006c0e0870038144fbd553a7ef43db3b6e892a9
Ashley Sawle <ads2202cu@gmail.com>
2020-09-01 16:08:35 +0100

    Unit test for peptideIntensityPlot

---
+++ tests/testthat/test-peptideIntensityPlot.R
@@ -0,0 +1,39 @@
+context("Peptide intensity plot")
+library(qPLEXanalyzer)
+
+MSnSet_data <- readRDS("convertToMSnset_oht_esr1_msnset.rds")
+MSnset_P <- readRDS("summarizeIntensities_msnset.rds")
+
+# standard
+plt1 <- peptideIntensityPlot(MSnSet_data, 
+                     combinedIntensities=MSnset_P, 
+                     ProteinID="P03372", 
+                     ProteinName= "ESR1")
+
+# no combined intensity
+plt2 <- peptideIntensityPlot(MSnSet_data, 
+                           ProteinID="P03372", 
+                           ProteinName= "ESR1")
+
+# selected sequence
+plt3 <- peptideIntensityPlot(MSnSet_data, 
+                             combinedIntensities=MSnset_P,
+                             ProteinID="P03372", 
+                             ProteinName= "ESR1",
+                             selectedSequence = "[K].GMEHLYSMK.[C]")
+
+# selected sequence & modification
+plt4 <- peptideIntensityPlot(MSnSet_data, 
+                             combinedIntensities=MSnset_P,
+                             ProteinID="P03372", 
+                             ProteinName= "ESR1",
+                             selectedSequence = "[K].GMEHLYSMK.[C]",
+                             selectedModifications = "2xTMT6plex [N-Term; K9]")
+
+
+test_that("Peptide plot works", {
+  vdiffr::expect_doppelganger("Peptide intensity plot", plt1)
+  vdiffr::expect_doppelganger("Peptide intensity plot no combined intensity", plt2)
+  vdiffr::expect_doppelganger("Peptide intensity plot with selected sequence", plt3)
+  vdiffr::expect_doppelganger("Peptide intensity plot with selected modification", plt4)
+})
