f64b0190631e0da9cecaf8d432c14f2b12e03d1a
Ashley Sawle <ads2202cu@gmail.com>
2020-09-04 13:13:27 +0100

    Reduce intensity matrix size to reduce reference file size

--- tests/testthat/test-mergePeptides.R
+++ tests/testthat/test-mergePeptides.R
@@ -1,37 +1,34 @@
 context("Merge Peptides")
 library(qPLEXanalyzer)
 
-# data(human_anno)
-# data(exp3_OHT_ESR1)
-# MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1, 
-#                                metadata = exp3_OHT_ESR1$metadata_qPLEX1,
-#                                indExpData = c(7:16), 
-#                                Sequences = 2, 
-#                                Accessions = 6)
-
-MSnSet_data <- readRDS("convertToMSnset_oht_esr1_msnset.rds")
-MSnSet_phos <- readRDS("convertToMSnset_phospho_msnset.rds")
 data(human_anno)
+data(exp3_OHT_ESR1)
+exp3Int <- exp3_OHT_ESR1$intensities_qPLEX1[1:2000, ]
+rawMSnSet <- convertToMSnset(exp3Int,
+                             metadata = exp3_OHT_ESR1$metadata_qPLEX1,
+                             indExpData = c(7:16),
+                             Sequences = 2,
+                             Accessions = 6)
+
+MSnSet_phos <- readRDS("convertToMSnset_phospho_msnset.rds")
+protMSnSet <- mergePeptides(rawMSnSet, 
+                            summarizationFunction = sum, 
+                            annotation = human_anno)
+
+phosProtMSnSet <- mergePeptides(MSnSet_phos, 
+                                summarizationFunction = sum, 
+                                annotation = human_anno, 
+                                keepCols = 7:8)
+phosProtMSnSetColname <- mergePeptides(MSnSet_phos, 
+                                summarizationFunction = sum, 
+                                annotation = human_anno, 
+                                keepCols = c("Positions in Master Proteins", 
+                                             "Modifications in Master Proteins"))
 
 test_that("Merge peptides works", {
-  expect_equal_to_reference(
-    mergePeptides(MSnSet_data, 
-                  summarizationFunction = sum, 
-                  annotation = human_anno), 
-    file="mergePeptides_msnset.rds")  
-  expect_equal_to_reference(
-      mergePeptides(MSnSet_phos, 
-                    summarizationFunction = sum, 
-                    annotation = human_anno, 
-                    keepCols = 7:8), 
-      file="mergePeptides_phos_msnset.rds")
-  expect_equal_to_reference(
-    mergePeptides(MSnSet_phos, 
-                  summarizationFunction = sum, 
-                  annotation = human_anno, 
-                  keepCols = c("Positions in Master Proteins", 
-                               "Modifications in Master Proteins")), 
-    file="mergePeptides_phos_msnset.rds")
+  expect_equal_to_reference(protMSnSet, file="mergePeptides_msnset.rds")  
+  expect_equal_to_reference(phosProtMSnSet, file="mergePeptides_phos_msnset.rds")
+  expect_equal_to_reference(phosProtMSnSetColname, file="mergePeptides_phos_msnset.rds")
 })
 
 
