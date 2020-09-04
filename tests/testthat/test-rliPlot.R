dde0859de761b9c75fdd785f4e8221341f133844
Ashley Sawle <ads2202cu@gmail.com>
2020-09-04 14:17:03 +0100

    Reduce intensity matrix size to reduce reference file size

--- tests/testthat/test-rliPlot.R
+++ tests/testthat/test-rliPlot.R
@@ -1,25 +1,24 @@
 context("RLI plot")
 library(qPLEXanalyzer)
 
-MSnSet_data <- readRDS("convertToMSnset_oht_esr1_msnset.rds")
-
-rliPlot(MSnSet_data, title = "qPLEX_RIME_ER")
-
-# custom colours
-customCols <- rainbow(length(unique(pData(MSnSet_data)$SampleGroup)))
-names(customCols) <- unique(pData(MSnSet_data)$SampleGroup)
-rliPlot(MSnSet_data, title = "qPLEX_RIME_ER", sampleColours = customCols)
+data(exp3_OHT_ESR1)
+exp3Int <- exp3_OHT_ESR1$intensities_qPLEX1[1:200, ]
+rawMSnSet <- convertToMSnset(exp3Int,
+                             metadata = exp3_OHT_ESR1$metadata_qPLEX1,
+                             indExpData = c(7:16),
+                             Sequences = 2,
+                             Accessions = 6)
 
 # default
-plt1 <- rliPlot(MSnSet_data, title = "qPLEX_RIME_ER")
+plt1 <- rliPlot(rawMSnSet, title = "qPLEX_RIME_ER")
 
 # colour by rep and include IgG
-plt2 <- rliPlot(MSnSet_data, omitIgG = FALSE, colourBy = "BioRep")
+plt2 <- rliPlot(rawMSnSet, omitIgG = FALSE, colourBy = "BioRep")
 
 # custom colours
-customCols <- rainbow(length(unique(pData(MSnSet_data)$SampleGroup)))
-names(customCols) <- unique(pData(MSnSet_data)$SampleGroup)
-plt3 <- rliPlot(MSnSet_data, sampleColours = customCols)
+customCols <- rainbow(length(unique(pData(rawMSnSet)$SampleGroup)))
+names(customCols) <- unique(pData(rawMSnSet)$SampleGroup)
+plt3 <- rliPlot(rawMSnSet, sampleColours = customCols)
 
 test_that("RLI plot works", {
   vdiffr::expect_doppelganger("RLI plot", plt1)
