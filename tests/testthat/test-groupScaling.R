07b032173ccf82678284268a92695806d0f7ddff
Ashley Sawle <ads2202cu@gmail.com>
2020-09-04 12:48:51 +0100

    Reduce intensity matrix size for test

--- tests/testthat/test-groupScaling.R
+++ tests/testthat/test-groupScaling.R
@@ -1,18 +1,16 @@
 context("Group Scaling normalizaion")
 library(qPLEXanalyzer)
 
-# data(exp2_Xlink)
-# 
-# MSnSet_data <- convertToMSnset(exp2_Xlink$intensities,
-#                                metadata = exp2_Xlink$metadata,
-#                                indExpData = c(7:16), 
-#                                Sequences = 2, 
-#                                Accessions = 6)
+data(exp2_Xlink)
 
-MSnSet_data <- readRDS("convertToMSnset_exp2_msnset.rds")
+exp2Int <- exp2_Xlink$intensities[1:50, ]
+rawMSnSet <- convertToMSnset(exp2Int,
+                             metadata = exp2_Xlink$metadata,
+                             indExpData = c(7:16),
+                             Sequences = 2,
+                             Accessions = 6)
+normMSnSet <- groupScaling(rawMSnSet)
 
 test_that("Group scaling normalization works", {
-  expect_equal_to_reference(
-    groupScaling(MSnSet_data), 
-    file="groupScaling_msnset.rds")
+  expect_equal_to_reference(normMSnSet, file="groupScaling_msnset.rds")
 })
