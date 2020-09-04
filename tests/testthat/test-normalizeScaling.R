051017e2e8b84ff5127f1f80402578ab7a06aec0
Ashley Sawle <ads2202cu@gmail.com>
2020-09-04 13:05:21 +0100

    Reduce intensity matrix size to reduce the reference files sizes

--- tests/testthat/test-normalizeScaling.R
+++ tests/testthat/test-normalizeScaling.R
@@ -1,27 +1,22 @@
 context("Scaling normalizaion")
 library(qPLEXanalyzer)
+data(exp2_Xlink)
 
-# data(exp2_Xlink)
-# 
-# MSnSet_data <- convertToMSnset(exp2_Xlink$intensities,
-#                                metadata = exp2_Xlink$metadata,
-#                                indExpData = c(7:16), 
-#                                Sequences = 2, 
-#                                Accessions = 6)
-
-MSnSet_data <- readRDS("convertToMSnset_exp2_msnset.rds")
+exp2Int <- exp2_Xlink$intensities[1:50, ]
+rawMSnSet <- convertToMSnset(exp2Int,
+                             metadata = exp2_Xlink$metadata,
+                             indExpData = c(7:16),
+                             Sequences = 2,
+                             Accessions = 6)
+normMSnSet <- normalizeScaling(rawMSnSet, scalingFunction = median)
+pnormMSnSet <- normalizeScaling(rawMSnSet, 
+                                scalingFunction = median, 
+                                ProteinId = "P04264")
 
 test_that("Scaling normalization works", {
-  expect_equal_to_reference(
-    normalizeScaling(MSnSet_data, 
-                     scalingFunction = median), 
-    file="normalizeScaling_msnset.rds")
+  expect_equal_to_reference(normMSnSet, file="normalizeScaling_msnset.rds")
 })
 
 test_that("Scaling normalization with reference to protein works", {
-  expect_equal_to_reference(
-    normalizeScaling(MSnSet_data, 
-                     scalingFunction = median, 
-                     ProteinId = "P03372"), 
-    file="normalizeScaling_protein_msnset.rds")
+  expect_equal_to_reference(pnormMSnSet, file="normalizeScaling_prot_msnset.rds")
 })
