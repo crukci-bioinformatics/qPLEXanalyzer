9c97a80566e5b9eeee4f3fb1ad5e9ed55cf0142c
Ashley Sawle <ads2202cu@gmail.com>
2020-09-04 13:21:14 +0100

    Reduce intensity matrix size to reduce reference file size

--- tests/testthat/test-summarizeIntensity.R
+++ tests/testthat/test-summarizeIntensity.R
@@ -1,19 +1,17 @@
 context("Summarize intensities to protein level")
 library(qPLEXanalyzer)
 
-# data(human_anno)
-# data(exp3_OHT_ESR1)
-# MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1, 
-#                                metadata = exp3_OHT_ESR1$metadata_qPLEX1,
-#                                indExpData = c(7:16), 
-#                                Sequences = 2, 
-#                                Accessions = 6)
-
-MSnSet_data <- readRDS("convertToMSnset_oht_esr1_msnset.rds")
 data(human_anno)
+data(exp3_OHT_ESR1)
+exp3Int <- exp3_OHT_ESR1$intensities_qPLEX1[1:2000, ]
+rawMSnSet <- convertToMSnset(exp3Int,
+                             metadata = exp3_OHT_ESR1$metadata_qPLEX1,
+                             indExpData = c(7:16),
+                             Sequences = 2,
+                             Accessions = 6)
+
+protMSnSet <- summarizeIntensities(rawMSnSet, sum, human_anno)
 
 test_that("Summarize intensities works", {
-  expect_equal_to_reference(
-    summarizeIntensities(MSnSet_data, sum, human_anno), 
-    file="summarizeIntensities_msnset.rds")
+  expect_equal_to_reference(protMSnSet, file="summarizeIntensities_msnset.rds")
 })
