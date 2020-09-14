0039cb474fcdbee31ac981e8785592220cf9abe6
Ashley Sawle <ads2202cu@gmail.com>
2020-09-14 16:02:52 +0100

    Format examples

--- R/normalizeScaling.R
+++ R/normalizeScaling.R
@@ -24,8 +24,11 @@
 #' 
 #' data(human_anno)
 #' data(exp3_OHT_ESR1)
-#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1, metadata=exp3_OHT_ESR1$metadata_qPLEX1,
-#' indExpData=c(7:16), Sequences=2, Accessions=6)
+#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1, 
+#'                                metadata=exp3_OHT_ESR1$metadata_qPLEX1,
+#'                                indExpData=c(7:16), 
+#'                                Sequences=2, 
+#'                                Accessions=6)
 #' MSnset_norm <- normalizeScaling(MSnSet_data, scalingFunction=median)
 #' 
 #' @export normalizeScaling
