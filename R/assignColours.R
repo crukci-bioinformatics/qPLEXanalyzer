4616418561147da62285120834c070a8c4abf7c0
Ashley Sawle <ads2202cu@gmail.com>
2020-09-14 13:02:19 +0100

    Add roxygen to assignColours

--- R/assignColours.R
+++ R/assignColours.R
@@ -1,6 +1,22 @@
-# Assigns colours
-## check this for selecting colors http://colorbrewer2.org/
-
+#' Assigns colours to samples in groups
+#' 
+#' Assigns colours to samples in groups for plotting
+#' 
+#' 
+#' @param MSnSetObj MSnSet; an object of class MSnSet
+#' @param colourBy character: column name from pData(MSnSetObj) to use for
+#' coloring samples
+#' @return A character vector of colors for samples.
+#' @examples
+#' 
+#' data(human_anno)
+#' data(exp3_OHT_ESR1)
+#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1,
+#' metadata=exp3_OHT_ESR1$metadata_qPLEX1,
+#' indExpData=c(7:16), Sequences=2, Accessions=6)
+#' sampleColours <- assignColours(MSnSet_data)
+#' 
+#' @export assignColours
 assignColours <- function(MSnSetObj, colourBy="SampleGroup") {
     if (!is(MSnSetObj, "MSnSet")) {
         stop("MSnSetObj has to be of class MSnSet..")
