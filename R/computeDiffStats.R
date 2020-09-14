3ac65d3af7b580e14d146af1468a610f7cc1b0bc
Ashley Sawle <ads2202cu@gmail.com>
2020-09-14 13:02:52 +0100

    Add roxygen to computeDiffStats

--- R/computeDiffStats.R
+++ R/computeDiffStats.R
@@ -6,6 +6,54 @@ log2xplus1 <- function(x) {
     log2(x + 1e-10)
 }
 
+
+
+#' Compute differential statistics
+#' 
+#' Compute differential statistics on the given contrasts, based on
+#' \code{\link{limma}} functions.
+#' 
+#' A statistical analysis for the identification of differentially regulated or
+#' bound proteins is carried out using limma based analysis. It uses linear
+#' models to assess differential expression in the context of multifactor
+#' designed experiments.  Firstly, a linear model is fitted for each protein
+#' where the model includes variables for each group and MS run. Then, log2
+#' fold changes between comparisions are estimated. Multiple testing correction
+#' of p-values are applied using the Benjamini-Hochberg method to control the
+#' false discovery rate (FDR).
+#' 
+#' In order to correct for batch effect, variable(s) can be defined. It should
+#' corresponds to a column name in pData(MSnSetObj). The default variable is
+#' "SampleGroup" that distinguish between two groups.  If more variables are
+#' defined they are added to default.
+#' 
+#' @param MSnSetObj MSnSet; An object of class MSnSet
+#' @param batchEffect character; vector of variable(s) to correct for batch
+#' effect, Default : "SampleGroup"
+#' @param transform logical; apply log2 transformation to the raw intensitites
+#' @param contrasts character; named character vector of contrasts for
+#' differential statistics
+#' @param trend logical; TRUE or FALSE
+#' @param robust logical; TRUE or FALSE
+#' @return A list object containing three components: MSnSetObj of class
+#' \code{MSnSet} (see \code{\link{MSnSet-class}}) object), fittedLM (fitted
+#' linear model) and fittedContrasts. This object should be input into
+#' getContrastResults function to get differential results.  See
+#' \code{\link{eBayes}} function of \code{\link{limma}} for more details on
+#' differential statistics.
+#' @examples
+#' 
+#' data(human_anno)
+#' data(exp3_OHT_ESR1)
+#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1,
+#' metadata=exp3_OHT_ESR1$metadata_qPLEX1,
+#' indExpData=c(7:16), Sequences=2, Accessions=6)
+#' MSnset_norm <- groupScaling(MSnSet_data, scalingFunction=median)
+#' MSnset_Pnorm <- summarizeIntensities(MSnset_norm, sum, human_anno)
+#' contrasts <- c(tam.24h_vs_vehicle = "tam.24h - vehicle", tam.6h_vs_vehicle = "tam.6h - vehicle")
+#' diffstats <- computeDiffStats(MSnSetObj=MSnset_Pnorm, contrasts=contrasts)
+#' 
+#' @export computeDiffStats
 computeDiffStats <- function(MSnSetObj, batchEffect = NULL, transform = TRUE, 
                              contrasts, trend = TRUE, robust = TRUE) {
     checkArg_computeDiffStats(MSnSetObj, batchEffect, transform, contrasts,
