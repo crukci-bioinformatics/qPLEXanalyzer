39d6b143bd190f50e6595f51040ef0b703d87552
Ashley Sawle <ads2202cu@gmail.com>
2020-09-14 15:39:19 +0100

    Add roxygen to pcaPlot

--- R/pcaPlot.R
+++ R/pcaPlot.R
@@ -1,4 +1,55 @@
 # PCA plot
+
+
+#' PCA plot
+#'
+#' PCA plots of the samples within MSnset
+#'
+#' The column provided to the "colourBy" argument will be used to colour the
+#' samples. The colours will be determined using the function
+#' \link{assignColours}, alternatively the user may specify a named vector of
+#' colours using the "sampleColours" argument. The names of the "sampleColours"
+#' vector should match the unique values in the "colourBy" column.
+#'
+#' @param MSnSetObj MSnSet; an object of class MSnSet
+#' @param omitIgG Logical: whether to remove IgG from the PCA plot
+#' @param sampleColours character: A named vector of colours for samples
+#' @param transFunc func: internal helper function for log transformation
+#' @param transform logical: whether to log transform intensities
+#' @param colourBy character: column name to use for colouring samples from
+#'   \code{pData(MSnSetObj)}
+#' @param title character: title for the plot
+#' @param labelColumn character: column name from \code{pData(MSnSetObj)} to use
+#'   for labelling points on the plot
+#' @param labelsize numeric: size of the labels
+#' @param pointsize numeric: size of plotting points
+#' @param x.nudge numeric: distance to move labels along the x-axis away from
+#'   the plotting points
+#' @param x.PC numeric: The principle component to plot on the x-axis; the
+#'   following PC will be plotted on the y-axis
+#' @return An instance of class "MSnSet".
+#' @examples
+#'
+#' data(human_anno)
+#' data(exp3_OHT_ESR1)
+#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1,
+#'                                metadata=exp3_OHT_ESR1$metadata_qPLEX1, 
+#'                                indExpData=c(7:16), 
+#'                                Sequences=2, 
+#'                                Accessions=6)
+#' exprs(MSnSet_data) <- exprs(MSnSet_data)+0.01
+#' pcaPlot(MSnSet_data, omitIgG = TRUE, labelColumn = "BioRep")
+#'
+#' # custom colours and PC2 v PC3
+#' customCols <- rainbow(length(unique(pData(MSnSet_data)$SampleGroup)))
+#' names(customCols) <- unique(pData(MSnSet_data)$SampleGroup)
+#' pcaPlot(MSnSet_data, 
+#'         omitIgG = TRUE, 
+#'         labelColumn = "BioRep", 
+#'         sampleColours = customCols, 
+#'         x.PC=2)
+#'
+#' @export pcaPlot
 pcaPlot <- function(MSnSetObj, omitIgG=FALSE, sampleColours=NULL,
                     transFunc=log2xplus1, transform=TRUE, 
                     colourBy="SampleGroup", title="", labelColumn="BioRep",
