ff46cb396fa77d317e7b8ec9f61e4057a1294f03
Ashley Sawle <ads2202cu@gmail.com>
2020-09-14 13:55:39 +0100

    Add roxygen to intensityPlot

--- R/intensityPlot.R
+++ R/intensityPlot.R
@@ -1,6 +1,45 @@
 # Intensity distribution plot
 ## intensities is a data frame containing columns for each sample
 ## sampleColours is a named vector that maps samples to colours
+
+
+#' Intensity Distribution Plot
+#' 
+#' Intensity distribution plot of all the samples
+#' 
+#' The column provided to the \code{colourBy} argument will be used to colour
+#' the samples. The colours will be determined using the function
+#' \code{\link{assignColours}}, alternatively the user may specify a named
+#' vector of colours using the \code{sampleColours} argument. The names of the
+#' \code{sampleColours} vector should match the unique values in the
+#' \code{colourBy} column.
+#' 
+#' @param MSnSetObj MSnSet; an object of class MSnSet
+#' @param sampleColours character: a vector of colors for samples
+#' @param title character: title for the plot
+#' @param colourBy character: column name from pData(MSnSetObj) to use for
+#' coloring samples
+#' @param transform logical: whether to log transform intensities
+#' @param xlab character: label for x-axis
+#' @param trFunc func: internal helper function for log transformation
+#' @return An intensity distribution plot for quantitative proteomics data.
+#' @examples
+#' 
+#' data(human_anno)
+#' data(exp3_OHT_ESR1)
+#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1, 
+#'                                metadata=exp3_OHT_ESR1$metadata_qPLEX1, 
+#'                                indExpData=c(7:16), 
+#'                                Sequences=2, 
+#'                                Accessions=6)
+#' intensityPlot(MSnSet_data, title = "qPLEX_RIME_ER")
+#' 
+#' # custom colours
+#' customCols <- rainbow(length(unique(pData(MSnSet_data)$SampleGroup)))
+#' names(customCols) <- unique(pData(MSnSet_data)$SampleGroup)
+#' intensityPlot(MSnSet_data, title = "qPLEX_RIME_ER", sampleColours = customCols)
+#' 
+#' @export intensityPlot
 intensityPlot <- function(MSnSetObj, sampleColours=NULL, title="", 
                           colourBy="SampleGroup", transform=TRUE, 
                           xlab="log2(intensity)", trFunc=log2xplus1) {
