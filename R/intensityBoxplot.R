4708d54434002423b38c61a309afd48a40a9bc9f
Ashley Sawle <ads2202cu@gmail.com>
2020-09-14 13:32:27 +0100

    Add roxygen to intensityBoxplot

--- R/intensityBoxplot.R
+++ R/intensityBoxplot.R
@@ -1,4 +1,43 @@
 # Intensity boxplots
+
+
+#' Intensity Distribution boxplot
+#' 
+#' Intensity distribution boxplot of all the samples
+#' 
+#' The column provided to the \code{colourBy} argument will be used to colour
+#' the samples. The colours will be determined using the function
+#' \code{\link{assignColours}}, alternatively the user may specify a named
+#' vector of colours using the \code{sampleColours} argument. The names of the
+#' \code{sampleColours} vector should match the unique values in the
+#' \code{colourBy} column.
+#' 
+#' @param MSnSetObj MSnSet; an object of class MSnSet
+#' @param title character; title of the plot
+#' @param sampleColours character: a named character vector of colors for
+#' samples
+#' @param colourBy character: column name from pData(MSnSetObj) to use for
+#' coloring samples
+#' @return An intensity distribution boxplot for quantitative proteomics data.
+#' @examples
+#' 
+#' data(human_anno)
+#' data(exp3_OHT_ESR1)
+#' MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1, 
+#'                                metadata=exp3_OHT_ESR1$metadata_qPLEX1,
+#'                                indExpData=c(7:16), 
+#'                                Sequences=2, 
+#'                                Accessions=6)
+#' intensityBoxplot(MSnSet_data, title = "qPLEX_RIME_ER")
+#' 
+#' # custom colours
+#' customCols <- rainbow(length(unique(pData(MSnSet_data)$SampleGroup)))
+#' names(customCols) <- unique(pData(MSnSet_data)$SampleGroup)
+#' intensityBoxplot(MSnSet_data, 
+#'                  title = "qPLEX_RIME_ER", 
+#'                  sampleColours = customCols)
+#' 
+#' @export intensityBoxplot
 intensityBoxplot <- function(MSnSetObj, title="", sampleColours=NULL, 
                              colourBy="SampleGroup") {
     if (!is(MSnSetObj, "MSnSet")) {
