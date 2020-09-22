e0ddd968bdf2db0e74d720b2515ac958e7dbba06
Ashley Sawle <ads2202cu@gmail.com>
2020-09-22 15:20:34 +0100

    Update code to make easier to read

--- R/groupScaling.R
+++ R/groupScaling.R
@@ -44,24 +44,25 @@ groupScaling <- function(MSnSetObj, scalingFunction=median,
                          groupingColumn="SampleGroup") {
     checkArg_groupScaling(MSnSetObj, scalingFunction, groupingColumn)
     
-    exprs(MSnSetObj) <- as.data.frame(exprs(MSnSetObj)) %>%
-        rownames_to_column("PeptideID") %>%
-        pivot_longer(names_to = "SampleName", values_to = "RawIntensity", -PeptideID) %>%
-        left_join(pData(MSnSetObj), "SampleName") %>%
-        group_by(SampleName) %>%
-        mutate(scaledIntensity = scalingFunction(RawIntensity) %>% log()) %>%
-        group_by(across(groupingColumn)) %>%
-        mutate(meanscaledIntensity = mean(scaledIntensity)) %>%
-        ungroup() %>%
-        mutate(scalingFactors = exp(scaledIntensity - meanscaledIntensity)) %>%
-        mutate(normalizedIntensities = RawIntensity / scalingFactors) %>%
-        select(PeptideID, SampleName, normalizedIntensities) %>%
-        pivot_wider(names_from = "SampleName", 
-                    values_from = "normalizedIntensities") %>%
-        arrange(factor(PeptideID, levels = rownames(MSnSetObj))) %>%
-        column_to_rownames("PeptideID") %>%
-        select(colnames(MSnSetObj)) %>%
-        as.matrix()
+    intensities <- as.data.frame(exprs(MSnSetObj))
+    
+    # get median/mean for each sample
+    scaledIntensities <- intensities %>%
+        summarize(across(everything(), scalingFunction)) %>%
+        mutate(across(everything(), log)) %>% 
+        pivot_longer(names_to = "SampleName", values_to="sInt", everything())
+    
+    # get group mean for each sample
+    meanScaledIntensities <- scaledIntensities %>% 
+        left_join(pData(MSnSetObj), by="SampleName") %>% 
+        group_by(across(groupingColumn)) %>% 
+        mutate(meanscaledIntensity = mean(sInt)) %>% 
+        pull(meanscaledIntensity)
+    
+    scaledIntensities <- pull(scaledIntensities, sInt)
+    scalingFactors <- exp(scaledIntensities - meanScaledIntensities)
+    normalizedIntensities <- t(t(intensities) / scalingFactors)
+    exprs(MSnSetObj) <- normalizedIntensities
     return(MSnSetObj)
 }
 
