07a0cadb612946398131b469cfed62a5b4a5d323
Ashley Sawle <ads2202cu@gmail.com>
2020-09-24 10:31:02 +0100

    change to example and title

--- tests/testthat/test-maVolPlot.R
+++ tests/testthat/test-maVolPlot.R
@@ -6,6 +6,11 @@ diffstats <- readRDS("computeDiffStats_msnset.rds")
 
 plt1 <- maVolPlot(diffstats, contrast = contrasts, plotType="MA")
 plt2 <- maVolPlot(diffstats, contrast = contrasts, plotType="Volcano")
+goi <- c("P48552", "Q96C36", "P54886", "P32322", "Q4ZG55", "P10644")
+plt3 <- maVolPlot(diffstats, 
+                  contrast = contrasts, 
+                  plotType="Volcano", 
+                  selectedGenes = goi)
 
 test_that("MA plot works", {
   vdiffr::expect_doppelganger("MA plot", plt1)
@@ -14,4 +19,9 @@ test_that("MA plot works", {
 
 test_that("Volcano plot works", {
   vdiffr::expect_doppelganger("Volcano plot", plt2)
+})
+
+
+test_that("Volcano plot with selected genes", {
+    vdiffr::expect_doppelganger("Volcano plot with selected genes", plt3)
 })
