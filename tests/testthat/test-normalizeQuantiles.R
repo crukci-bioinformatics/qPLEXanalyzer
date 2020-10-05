3f6c22f4ed33a39563ace0cacc23c1fd7790b1ac
Ashley Sawle <ads2202cu@gmail.com>
2020-10-05 11:20:28 +0100

    Remove MSnSet from reference object

--- tests/testthat/test-normalizeQuantiles.R
+++ tests/testthat/test-normalizeQuantiles.R
@@ -2,7 +2,7 @@ context("Quantile normalizaion")
 library(qPLEXanalyzer)
 data(exp2_Xlink)
 
-exp2Int <- exp2_Xlink$intensities[1:50, ]
+exp2Int <- exp2_Xlink$intensities[1:1000, ]
 rawMSnSet <- convertToMSnset(exp2Int,
                              metadata = exp2_Xlink$metadata,
                              indExpData = c(7:16),
@@ -10,7 +10,17 @@ rawMSnSet <- convertToMSnset(exp2Int,
                              Accessions = 6)
 normMSnSet <- normalizeQuantiles(rawMSnSet)
 
+# # The MSnSet object contains the MSnbase version in teh `processingData` slot
+# # This will cause the test to fail if the MSnbase version used in the build
+# test_that("Quantile normalization works", {
+#   expect_equal_to_reference(normMSnSet, file="normalizeQuantiles_msnset.rds")
+# })
+
+# The function only changes the expression set, so let's just compare that
+
+testObj <-  exprs(normMSnSet)
 
 test_that("Quantile normalization works", {
-  expect_equal_to_reference(normMSnSet, file="normalizeQuantiles_msnset.rds")
+    expect_equal_to_reference(testObj, 
+                              file="normalizeQuantiles_NormalisedIntensities.rds")
 })
