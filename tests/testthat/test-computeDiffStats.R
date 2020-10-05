0c65bf54d522a0fdc1bf5846b8833f9c0399e840
Ashley Sawle <ads2202cu@gmail.com>
2020-10-05 13:00:38 +0100

    Update test - remove MSnBase, build own data object from raw

--- tests/testthat/test-computeDiffStats.R
+++ tests/testthat/test-computeDiffStats.R
@@ -1,23 +1,38 @@
 context("Compute differential analysis statistics")
 library(qPLEXanalyzer)
 
-# data(human_anno)
-# data(exp3_OHT_ESR1)
-# MSnSet_data <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1,
-#                                metadata = exp3_OHT_ESR1$metadata_qPLEX1,
-#                                indExpData = c(7:16), 
-#                                Sequences = 2, 
-#                                Accessions = 6)
-# MSnset_norm <- groupScaling(MSnSet_data, scalingFunction = median)
-# MSnset_Pnorm <- summarizeIntensities(MSnset_norm, sum, human_anno)
+data(human_anno)
+data(exp3_OHT_ESR1)
+rawMSnSet <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1[1:1000,],
+                               metadata = exp3_OHT_ESR1$metadata_qPLEX1,
+                               indExpData = c(7:16),
+                               Sequences = 2,
+                               Accessions = 6)
+MSnset_norm <- groupScaling(rawMSnSet, scalingFunction = median)
+MSnset_Pnorm <- summarizeIntensities(MSnset_norm, sum, human_anno)
 
-MSnset_Pnorm <- readRDS("summarizeIntensities_msnset.rds")
 contrasts <- c(tam.24h_vs_vehicle = "tam.24h - vehicle", 
                tam.6h_vs_vehicle = "tam.6h - vehicle")
+diffstats <- computeDiffStats(MSnSetObj=MSnset_Pnorm, contrasts=contrasts)
 
+# # The MSnSet object contains the MSnbase version in the `processingData` slot
+# # This will cause the test to fail if the MSnbase version used in the build
+# # changes
+# test_that("Compute Diff Stats works", {
+#   expect_equal_to_reference(diffstats, file="computeDiffStats_msnset.rds")
+# })
+
+# The function creates:
+# diffstats <- list(MSnSetObj = MSnSetObj, 
+#                   fittedLM = fit, 
+#                   fittedContrasts = fittedContrasts))
+# Where the MSnSetObj is unchanged from input.
+# Let's just compare the `fitted` objects
+
+
+testObj <- list(fittedLM = diffstats$fittedLM,
+                fittedContrasts = diffstats$fittedContrasts)
 
 test_that("Compute Diff Stats works", {
-  expect_equal_to_reference(
-    computeDiffStats(MSnSetObj=MSnset_Pnorm, contrasts=contrasts), 
-    file="computeDiffStats_msnset.rds")
+  expect_equal_to_reference(testObj, file="computeDiffStats.rds")
 })
