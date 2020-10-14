a08fda080e278bc06a253700f4a702ecf49f0845
Ashley Sawle <ads2202cu@gmail.com>
2020-10-14 12:02:29 +0100

    Add checks for corrPlot

--- tests/testthat/test-corrPlot.R
+++ tests/testthat/test-corrPlot.R
@@ -8,6 +8,8 @@ rawMSnSet <- convertToMSnset(exp3_OHT_ESR1$intensities_qPLEX1,
                                Sequences = 2,
                                Accessions = 6)
 
+# test the function
+
 test_that("corrPlot works", {
   vdiffr::expect_doppelganger(
     "correlation plot default colour", 
@@ -23,4 +25,18 @@ test_that("corrPlot works", {
     "correlation plot change limit",
     corrPlot(rawMSnSet, addValues=TRUE, title="Correlation plot",
              low_cor_limit=0.5, high_cor_limit=1))
+})
+
+# test the argument checks
+
+test_that("argument checks - MSnset", {
+  expect_error(corrPlot(1), regexp = "MSnSetObj has to be of class MSnSet")
+})
+test_that("argument checks - low_cor_colour", {
+  expect_error(corrPlot(rawMSnSet, low_cor_colour = "wibble"), 
+               regexp = "low_cor_colour: wibble is not a valid colour.")
+})
+test_that("argument checks - high_cor_colour", {
+  expect_error(corrPlot(rawMSnSet, high_cor_colour = "wibble"), 
+               regexp = "high_cor_colour: wibble is not a valid colour.")
 })
