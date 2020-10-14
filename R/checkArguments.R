13a503fce7ecf40020af28676caec125bb30b845
Ashley Sawle <ads2202cu@gmail.com>
2020-10-14 10:53:30 +0100

    reorganise the validation functions

--- R/checkArguments.R
+++ R/checkArguments.R
@@ -7,53 +7,9 @@
 #' @importFrom purrr map_lgl
 #' @importFrom stringr str_c str_detect
 
-# check string is a valid colour ####
+## Checks on raw data ##########################################################
 
-is_validColour <- function(colour){
-  tests <- map_lgl(colour, ~tryCatch(is.matrix(col2rgb(.x)),
-                                     error = function(e) FALSE))
-  all(tests)
-}
-on_failure(is_validColour) <- function(call, env) {
-  varNam <- deparse(call$colour)
-  colour <- eval(call$colour, env)
-  tests <- map_lgl(colour, ~tryCatch(is.matrix(col2rgb(.x)),
-                                     error = function(e) FALSE))
-  str_c(
-    str_c(varNam, ": ", colour[!tests], " is not a valid colour."),
-    collapse="\n       ")
-}
-
-# check sample colours for plotting ####
-is_validSampleColours <- function(sampleColours, colourBy, MSnSetObj){
-  assert_that(is_validColour(sampleColours))
-  colnm <- names(sampleColours)
-  colbynm <- pData(MSnSetObj)[[colourBy]] %>% unique()
-  assert_that(!is.null(colnm), 
-              msg = str_c("sampleColours should be a named vector, where the ",
-                          "names are values in the ", colourBy, " column of ",
-                          "the MSnSet object"))
-  assert_that(length(colbynm)==length(colnm),
-              msg = str_c("sampleColours should provide a colour for each ",
-                          "value in the ", colourBy, " column of the MSnSet ",
-                          "object"))
-  assert_that(all(colbynm%in%colnm) && all(colnm%in%colbynm),
-              msg = str_c("The names of sampleColours do not match the values ",
-                          "in the ", colourBy, " column of the MSnSet object"))
-}
-
-
-# check metadata ####
-is_validMetadata <- function(metadata){
-    assert_that(is.data.frame(metadata))
-    columns <- c("SampleName","SampleGroup","BioRep","TechRep")
-    all(columns%in%colnames(metadata))
-}
-on_failure(is_validMetadata) <- function(call, env) {
-    "Metadata must have columns SampleName, SampleGroup, BioRep, and TechRep"
-}
-
-# check the sample dat ####
+# check the sample intensity data ####
 is_validSampleData <- function(ExpObj, metadata, indExpData){
     sampleData <- ExpObj[, indExpData]
     assert_that(all(colnames(sampleData)%in%metadata$SampleName)&
@@ -81,15 +37,7 @@ on_failure(is_validSequencesColumn) <- function(call, env){
     "Sequences should to be NULL when type is protein"
 }
 
-# check MSnSetObj ####
-is_MSnSet <- function(MSnSetObj){
-    is(MSnSetObj, "MSnSet")
-}
-on_failure(is_MSnSet) <- function(call, env){
-    "MSnSetObj has to be of class MSnSet"
-}
-
-# check the annotation table ####
+# check the annotation table
 is_validAnnotationData <- function(annotation){
     assert_that(is.data.frame(annotation))
     columns <- c("Accessions", "Gene", "Description", "GeneSymbol")
@@ -100,7 +48,17 @@ on_failure(is_validAnnotationData) <- function(call, env) {
            "Accessions, Gene, Description and GeneSymbol")
 }
 
-# check MSnSetOBj is a peptide level data set ####
+## Checks on the MSnSet object #################################################
+
+# check MSnSetObj
+is_MSnSet <- function(MSnSetObj){
+    is(MSnSetObj, "MSnSet")
+}
+on_failure(is_MSnSet) <- function(call, env){
+    "MSnSetObj has to be of class MSnSet"
+}
+
+# check MSnSet Object is a peptide level data set ####
 is_PeptideSet <- function(MSnSetObj){
     has_name(fData(MSnSetObj), "Sequences")
 }
@@ -108,18 +66,17 @@ on_failure(is_PeptideSet) <- function(call, env){
     "This MSnSet is not a peptide data set"
 }
 
-# Check scaling function is appropriate ####
-is_validScalingFunction <- function(scalingFunction){
-    assert_that(is.function(scalingFunction))
-    are_equal(scalingFunction, BiocGenerics::mean) |
-        are_equal(scalingFunction, mean) | 
-        are_equal(scalingFunction, median)
+# check metadata
+is_validMetadata <- function(metadata){
+    assert_that(is.data.frame(metadata))
+    columns <- c("SampleName","SampleGroup","BioRep","TechRep")
+    all(columns%in%colnames(metadata))
 }
-on_failure(is_validScalingFunction) <- function(call, env){
-    "scalingFunction should be mean or median'"
+on_failure(is_validMetadata) <- function(call, env) {
+    "Metadata must have columns SampleName, SampleGroup, BioRep, and TechRep"
 }
 
-# check the provided protein ID is in the MSnSetObj ####
+# check the provided protein ID is in the MSnSetObj
 is_validProteinId <- function(ProteinID, MSnSetObj, allowNull=TRUE){
     if(allowNull){
         assert_that(is.character(ProteinID) | is.null(ProteinID),
@@ -134,7 +91,7 @@ on_failure(is_validProteinId) <- function(call, env){
     "The ProteinID provided is not found the MSnset feature data"
 }
 
-# check a metadata column given in `varName` exists ####
+# check a metadata column given in `varName` exists
 is_validMetadataColumn <- function(metaColumn, MSnSetObj){
     metaColumn%in%colnames(pData(MSnSetObj))
 }
@@ -144,72 +101,64 @@ on_failure(is_validMetadataColumn) <- function(call, env){
     str_c(varNam, ": column ", colNam, " not found in the MSnset metadata")
 }
 
-# check the summarisation function is appropriate ####
-is_validSummarizationFunction <- function(summarizationFunction){
-    assert_that(is.function(summarizationFunction))
-    length(summarizationFunction(seq(10))) == 1
-}
-on_failure(is_validSummarizationFunction) <- function(call, env){
-    "summarizationFunction should be a summary function, e.g. mean or sum'"
-}
 
 # check the column indices are valid ####
 is_validColNumber <- function(cols, tab){
-  max(cols) <= ncol(tab)
+    max(cols) <= ncol(tab)
 }
 
 # check the column names are valid ####
 is_validColName <- function(cols, tab){
-  all(cols%in%colnames(tab))
+    all(cols%in%colnames(tab))
 }
 
 # check if the columns are in the fData ####
 is_validfDataColumn <- function(cols, MSnSetObj){
-  tab <- fData(MSnSetObj)
-  errMsg1 <- str_c("keepCols should be NULL, a character vector of column ",
-                   "names or a numeric vector of column indices.")
-  errMsg2 <- str_c("One or more of keepCols exceeds the number of columns in ",
-                   "fData(MSnSetObj).")
-  errMsg3 <- "One or more of keepCols is not found in fData(MSnSetObj)."
-  assert_that(is.numeric(cols) | is.character(cols) | is.null(cols),
-              msg = errMsg1)
-  if(is.numeric(cols)){ 
-    assert_that(is_validColNumber(cols, tab), msg = errMsg2) 
+    tab <- fData(MSnSetObj)
+    errMsg1 <- str_c("keepCols should be NULL, a character vector of column ",
+                     "names or a numeric vector of column indices.")
+    errMsg2 <- str_c("One or more of keepCols exceeds the number of columns in ",
+                     "fData(MSnSetObj).")
+    errMsg3 <- "One or more of keepCols is not found in fData(MSnSetObj)."
+    assert_that(is.numeric(cols) | is.character(cols) | is.null(cols),
+                msg = errMsg1)
+    if(is.numeric(cols)){ 
+        assert_that(is_validColNumber(cols, tab), msg = errMsg2) 
     }
-  if(is.character(cols)){ 
-    assert_that(is_validColName(cols, tab), msg = errMsg3) 
+    if(is.character(cols)){ 
+        assert_that(is_validColName(cols, tab), msg = errMsg3) 
     }
-  TRUE
+    TRUE
 }
 
 # check selected peptide sequences are in the fData for the specified protein ##
 are_validSequences <- function(Sequence, MSnSetObj, ProteinID){
-  assert_that(is.character(Sequence) | is.null(Sequence),
+    assert_that(is.character(Sequence) | is.null(Sequence),
                 msg="selectedSequence should be either a character or NULL")
-  subMSnSet <- MSnSetObj[fData(MSnSetObj)$Accessions==ProteinID,]
-  is.null(Sequence) || Sequence%in%fData(subMSnSet)$Sequences
+    subMSnSet <- MSnSetObj[fData(MSnSetObj)$Accessions==ProteinID,]
+    is.null(Sequence) || Sequence%in%fData(subMSnSet)$Sequences
 }
 on_failure(are_validSequences) <- function(call, env){
-  varNam <- deparse(call$Sequence)
-  protAcc <- eval(call$ProteinID, env)
-  str_c(varNam, 
-        ": The sequence(s) provided can not be found in the feature data for ",
-        protAcc)
+    varNam <- deparse(call$Sequence)
+    protAcc <- eval(call$ProteinID, env)
+    str_c(varNam, 
+          ": The sequence(s) provided can not be found in the feature data for ",
+          protAcc)
 }
 
 # check a selected modification is in the fData for the specified protein ######
 are_validModifications <- function(Modifications, MSnSetObj, ProteinID){
-  assert_that(is.character(Modifications) | is.null(Modifications),
-              msg="selectedModifications should be either a character or NULL")
-  subMSnSet <- MSnSetObj[fData(MSnSetObj)$Accessions==ProteinID,]
-  is.null(Modifications) || all(Modifications%in%fData(subMSnSet)$Modifications)
+    assert_that(is.character(Modifications) | is.null(Modifications),
+                msg="selectedModifications should be either a character or NULL")
+    subMSnSet <- MSnSetObj[fData(MSnSetObj)$Accessions==ProteinID,]
+    is.null(Modifications) || all(Modifications%in%fData(subMSnSet)$Modifications)
 }
 on_failure(are_validModifications) <- function(call, env){
-  varNam <- deparse(call$Modification)
-  protAcc <- eval(call$ProteinID, env)
-  str_c(varNam, 
-        ": the modification(s) provided can not be found in the feature data ",
-        "for ", protAcc)
+    varNam <- deparse(call$Modification)
+    protAcc <- eval(call$ProteinID, env)
+    str_c(varNam, 
+          ": the modification(s) provided can not be found in the feature data ",
+          "for ", protAcc)
 }
 
 # check the control column index ####
@@ -240,7 +189,33 @@ on_failure(is_validBatchEffect) <- function(call, env){
     "batchEffect includes columns not found the MSnset metadata"
 }
 
-# check list is a computeDiffStats output ####
+## Checks for scaling functions ################################################
+
+# Check scaling function is appropriate
+is_validScalingFunction <- function(scalingFunction){
+    assert_that(is.function(scalingFunction))
+    are_equal(scalingFunction, BiocGenerics::mean) |
+        are_equal(scalingFunction, mean) | 
+        are_equal(scalingFunction, median)
+}
+on_failure(is_validScalingFunction) <- function(call, env){
+    "scalingFunction should be mean or median'"
+}
+
+## Checks for summarisation functions ##########################################
+
+# check the summarisation function is appropriate
+is_validSummarizationFunction <- function(summarizationFunction){
+    assert_that(is.function(summarizationFunction))
+    length(summarizationFunction(seq(10))) == 1
+}
+on_failure(is_validSummarizationFunction) <- function(call, env){
+    "summarizationFunction should be a summary function, e.g. mean or sum'"
+}
+
+## Checks on diffstats object ##################################################
+
+# check list is a computeDiffStats output
 is_validDiffstats <- function(diffstats){
     c("MSnSetObj", "fittedLM", "fittedContrasts") %>% 
         map_lgl(has_name, x=diffstats) %>% 
@@ -250,7 +225,7 @@ on_failure(is_validDiffstats) <- function(call, env){
     "diffstats is not a valid output of the computeDiffStats function"
 }
 
-# check contrast requested is valid ####
+# check contrast requested is valid
 is_validContrast <- function(contrast, diffstats){
     assert_that(is.string(contrast))
     contrasts_available <- colnames(diffstats$fittedContrasts$coefficients)
@@ -266,7 +241,7 @@ on_failure(is_validContrast) <- function(call, env){
           "        ", contr_avail)
 }
 
-# check the control group #####
+# check the control group
 is_validControlGroup <- function(controlGroup, diffstats){
     assert_that(is.string(controlGroup) | is.null(controlGroup),
                 msg = paste0("controlGroup has to be a string ", 
@@ -280,4 +255,41 @@ on_failure(is_validControlGroup) <- function(call, env){
     str_c(argNam, ": '", ctrlGrp, "'",
            " is not found in the diffstats object. It should be one ", 
            "of the SampleGroups in the original MSnSet object.")
+}
+
+## Checks for plotting functions ###############################################
+
+# check string is a valid colour
+
+is_validColour <- function(colour){
+    tests <- map_lgl(colour, ~tryCatch(is.matrix(col2rgb(.x)),
+                                       error = function(e) FALSE))
+    all(tests)
+}
+on_failure(is_validColour) <- function(call, env) {
+    varNam <- deparse(call$colour)
+    colour <- eval(call$colour, env)
+    tests <- map_lgl(colour, ~tryCatch(is.matrix(col2rgb(.x)),
+                                       error = function(e) FALSE))
+    str_c(
+        str_c(varNam, ": ", colour[!tests], " is not a valid colour."),
+        collapse="\n       ")
+}
+
+# check sample colours for plotting
+is_validSampleColours <- function(sampleColours, colourBy, MSnSetObj){
+    assert_that(is_validColour(sampleColours))
+    colnm <- names(sampleColours)
+    colbynm <- pData(MSnSetObj)[[colourBy]] %>% unique()
+    assert_that(!is.null(colnm), 
+                msg = str_c("sampleColours should be a named vector, where the ",
+                            "names are values in the ", colourBy, " column of ",
+                            "the MSnSet object"))
+    assert_that(length(colbynm)==length(colnm),
+                msg = str_c("sampleColours should provide a colour for each ",
+                            "value in the ", colourBy, " column of the MSnSet ",
+                            "object"))
+    assert_that(all(colbynm%in%colnm) && all(colnm%in%colbynm),
+                msg = str_c("The names of sampleColours do not match the values ",
+                            "in the ", colourBy, " column of the MSnSet object"))
 }
