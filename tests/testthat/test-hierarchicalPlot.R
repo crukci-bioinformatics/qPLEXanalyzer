612864eb53c5d73a014066003458f4c3ee9f5d33
Ashley Sawle <ads2202cu@gmail.com>
2020-10-15 12:41:30 +0100

    add argument checks for hierarchicalPlot

--- tests/testthat/test-hierarchicalPlot.R
+++ tests/testthat/test-hierarchicalPlot.R
@@ -19,9 +19,40 @@ plt3 <- hierarchicalPlot(rawMSnSet, sampleColours=myCols)
 # Vertical version
 plt4 <- hierarchicalPlot(rawMSnSet, horizontal = FALSE)
 
+# test the function
+
 test_that("hierarchicalPlot works", {
   vdiffr::expect_doppelganger("Hierarchical Plot", plt1)
   vdiffr::expect_doppelganger("Hierarchical Plot colour by rep", plt2)
   vdiffr::expect_doppelganger("Hierarchical Plot custom colour", plt3)
   vdiffr::expect_doppelganger("Hierarchical Plot veritcal", plt4)
+})
+
+# test the argument checks
+
+test_that("argument checks - MSnset", {
+  expect_error(hierarchicalPlot(MSnSetObj = 1),
+               regexp = "MSnSetObj has to be of class MSnSet")
+})
+test_that("argument checks - colourBy", {
+  expect_error(hierarchicalPlot(MSnSetObj = rawMSnSet, colourBy = 1), 
+               regexp = "colourBy is not a string")
+  expect_error(hierarchicalPlot(MSnSetObj = rawMSnSet, colourBy = "Wibble"), 
+               regexp = "column Wibble not found in the MSnset metadata")
+})
+test_that("argument checks - sampleColours", {
+  myCols <- assignColours(rawMSnSet)
+  myCols[2] <- "blueq"
+  expect_error(hierarchicalPlot(MSnSetObj = rawMSnSet, sampleColours = myCols),
+               regexp = "sampleColours: blueq is not a valid colour")
+  myCols <- c("red", "blue", "green", "pink")
+  expect_error(hierarchicalPlot(MSnSetObj = rawMSnSet, sampleColours = myCols),
+               regexp = "sampleColours should be a named vector")
+  myCols <- assignColours(rawMSnSet)[-1]
+  expect_error(hierarchicalPlot(MSnSetObj = rawMSnSet, sampleColours = myCols),
+               regexp = "sampleColours should provide a colour for each")
+  myCols <- assignColours(rawMSnSet)
+  names(myCols)[1] <- "wibble"
+  expect_error(hierarchicalPlot(MSnSetObj = rawMSnSet, sampleColours = myCols),
+               regexp = "The names of sampleColours do not match the values")
 })
