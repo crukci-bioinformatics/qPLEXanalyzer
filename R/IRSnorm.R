bba1b95fc8978768d74c65a4284b34c0e84c85d3
Ashley Sawle <ads2202cu@gmail.com>
2021-04-15 12:31:27 +0100

    Some Code tidying

--- R/IRSnorm.R
+++ R/IRSnorm.R
@@ -91,22 +91,21 @@ checkArg_IRSnorm <- function(MSnSetObj, IRSname, groupingColumn){
 #' @export IRSnorm
 
 
-IRSnorm <- function(MSnSetObj, IRSname="RefPool", 
-                         groupingColumn="Plex") {
+IRSnorm <- function(MSnSetObj, IRSname="RefPool", groupingColumn="Plex") {
   checkArg_IRSnorm(MSnSetObj, IRSname, groupingColumn)
   
-  Ref_Set <- MSnSetObj[,which(pData(MSnSetObj)$SampleGroup==IRSname)]
+  Ref_Set <- MSnSetObj[,pData(MSnSetObj)$SampleGroup==IRSname]
   plex_id <- unique(pData(MSnSetObj)[,groupingColumn])
-  allgrps <- split(Ref_Set,groupingColumn)
-  grpMean <-  BiocGenerics::lapply(allgrps, function(x) {apply(exprs(x),1,mean)})
+  allgrps <- split(Ref_Set, groupingColumn)
+  grpMean <- BiocGenerics::lapply(allgrps, function(x) {apply(exprs(x), 1, mean)})
   irs_data <- matrix(unlist(grpMean), ncol = length(grpMean), byrow = FALSE)
-  irs_data_geomean <- apply(irs_data, 1, function(x) exp(mean(log(x))))
+  irs_data_geomean <- apply(irs_data, 1, function(x) {exp(mean(log(x)))})
   irs_factors <- irs_data_geomean/irs_data
 
   for (i in 1:length(plex_id))
   {
-    exprs(MSnSetObj)[,which(pData(MSnSetObj)[,groupingColumn]==i)] <- 
-      exprs(MSnSetObj)[,which(pData(MSnSetObj)[,groupingColumn]==i)] * irs_factors[,i]
+    plexCols <- which(pData(MSnSetObj)[,groupingColumn]==i)
+    exprs(MSnSetObj)[,plexCols] <- exprs(MSnSetObj)[,plexCols] * irs_factors[,i]
   }
   return(MSnSetObj)
 }
